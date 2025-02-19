ff711de0ea569e17761d8ca7fecb7d6fe47dcafc
Ckwasnik <kwasnik@live.com>
2025-02-18 19:12:03 -0800

    1

--- CIT3495_Project1/enter_data_service/app.py
+++ CIT3495_Project1/enter_data_service/app.py
@@ -71,4 +71,4 @@ def get_entries():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=3001, debug=True)
